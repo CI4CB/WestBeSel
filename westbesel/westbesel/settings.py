b589114fc0252da90e8770ac4ac00c9e97b54512
Carlos Peña <carlos.pena@heig-vd.ch>
2019-02-21 13:57:44 +0100

    Update settings.py

--- westbesel/westbesel/settings.py
+++ westbesel/westbesel/settings.py
@@ -17,7 +17,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 # See https://docs.djangoproject.com/en/1.6/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'w5-iqfj^5cl6tj0_8*xmgnj5r^@&p@n+)=6g1-!^i0&sohyicg'
+SECRET_KEY = 'SECRETSECRETSECRET'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
